b18089fb8926cd11c8985f775f8839436f8bdbfa
uzairdev12 <uzairmanan34@gmail.com>
2024-07-08 23:37:46 +0500

    Update Dashboard.jsx

--- Client/src/Pages/Dashboard.jsx
+++ Client/src/Pages/Dashboard.jsx
@@ -102,7 +102,7 @@ const Dashboard = () => {
               onClick={() => {
                 if (verified) {
                   navigator.clipboard.writeText(
-                    `https://probusinessapi.vercel.app/login/${user._id}`
+                    `https://theprobusiness.vercel.app/login/${user._id}`
                   );
                   toast.success("Refferral link copied to clipboard");
                 } else {
