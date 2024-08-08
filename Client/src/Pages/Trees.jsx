ce77f474d50f32a196d3b7f554bc92ed19faad36
uzairdev12 <uzairmanan34@gmail.com>
2024-08-08 09:53:01 +0500

    Update Trees.jsx

--- Client/src/Pages/Trees.jsx
+++ Client/src/Pages/Trees.jsx
@@ -2,15 +2,19 @@ import React, { useEffect, useState } from "react";
 import "./trees.css";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { TailSpin } from "react-loader-spinner";
-import { useNavigate } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { toast } from "sonner";
 
 const Trees = () => {
   const navigate = useNavigate();
 
-  const id = localStorage.getItem("userID");
+  const { id } = useParams();
   const [user, setUser] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [leftUserId, setLeftUserId] = useState("");
+  const [rightUserId, setRightUserId] = useState("");
+  const [selected, setSelected] = useState("");
+  const [showopt, setShowopt] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,7 +24,7 @@ const Trees = () => {
           setLoading(false);
         } else {
           const user = await fetch(
-            "https://probusinessapi.vercel.app/api/auth/userDetails",
+            "https://theprobusiness.vercel.app/api/auth/userDetails",
             {
               method: "POST",
               headers: {
@@ -38,6 +42,8 @@ const Trees = () => {
             return;
           }
           setUser(parsedData.user);
+          setLeftUserId(parsedData.leftuserid);
+          setRightUserId(parsedData.rightuserid);
           setLoading(false);
         }
       } catch (e) {
@@ -47,7 +53,7 @@ const Trees = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [id]);
 
   return (
     <div className="treesPage">
@@ -78,20 +84,70 @@ const Trees = () => {
             <h2>{user.name}</h2>
             <div className="trees">
               <div className="lefttree">
-                <p>
+                <p
+                  onClick={() => {
+                    if (leftUserId) {
+                      setSelected(leftUserId);
+                      setShowopt(true);
+                    } else {
+                      toast.error(
+                        "The user either does not exist or hasn't deposited yet. "
+                      );
+                    }
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
                   <span>{user.leftuser}</span>
                 </p>
                 <p>{user.leftusers}</p>
               </div>
               <div className="righttree">
                 <p>
-                  <span>{user.rightuser}</span>
+                  <span
+                    onClick={() => {
+                      if (rightUserId) {
+                        setSelected(rightUserId);
+                        setShowopt(true);
+                      } else {
+                        toast.error(
+                          "The user either does not exist or hasn't deposited yet. "
+                        );
+                      }
+                    }}
+                    style={{ cursor: "pointer" }}
+                  >
+                    {user.rightuser}
+                  </span>
                 </p>
                 <p>{user.rightusers}</p>
               </div>
             </div>
           </div>
         </>
+      )}
+      {showopt && (
+        <div className="treewrapper">
+          <button
+            onClick={() => {
+              navigate(`/trees/${selected}`);
+              setShowopt(false);
+            }}
+          >
+            Go to trees
+          </button>
+          <button
+            onClick={() => {
+              navigator.clipboard.writeText(
+                `https://theprobusiness.vercel.app/login/${user._id}`
+              );
+              toast.success("copied to clipboard");
+              setShowopt(false);
+            }}
+          >
+            copy reffer link
+          </button>
+          <button onClick={() => setShowopt(false)}>Cancel</button>
+        </div>
       )}
     </div>
   );
